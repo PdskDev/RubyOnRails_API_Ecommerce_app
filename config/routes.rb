81460bb3148c57069d0cf26fdabef10339f925b0
NadetDev <nadetdev@gmail.com>
2023-11-11 22:54:03 +0100

    set routes format constraints

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
- 
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  
+  # get "up" => "rails/health#show", as: :rails_health_check
+  #API definition
+  namespace :api, default : {format: :json} do
+  #Ex:- :default =>'' do
+    # resource list
+  end 
 end
