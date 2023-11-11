20a872caae61a3e99b91e0b227b89b3451e842c2
NadetDev <nadetdev@gmail.com>
2023-11-11 23:03:10 +0100

    api route versioning

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # get "up" => "rails/health#show", as: :rails_health_check
   #API definition
   namespace :api, default : {format: :json} do
-  #Ex:- :default =>'' do
+    namespace :v1 do
     # resource list
+    end
   end 
 end
