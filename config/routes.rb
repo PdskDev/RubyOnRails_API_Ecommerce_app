cc30ddaca33c98b35d49a9ceb640d8995561ea35
NadetDev <nadetdev@gmail.com>
2023-11-12 19:30:20 +0100

    model contraintes + migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # get "up" => "rails/health#show", as: :rails_health_check
   #API definition
-  namespace :api, defaults : {format: :json} do
+  namespace :api, defaults: {format: :json} do
     namespace :v1 do
     # resource list
     end
