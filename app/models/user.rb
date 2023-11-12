edda00de167bc73e658faafb65690b4811a01b4a
NadetDev <nadetdev@gmail.com>
2023-11-12 20:02:22 +0100

    User model validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
+EMAIL_REGEX =^[\\w!#$%&'*+/=?`{|}~^-]+(?:\\.[\\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$
+
 class User < ApplicationRecord
+    validates :email, uniqueness: true
+    validates_format_of :email, with: EMAIL_REGEX, message: "Invalid email format"
+    validates :password_digest, presence: true, length: { minimum: 6}
 end
