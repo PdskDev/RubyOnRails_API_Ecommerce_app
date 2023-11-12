9a4d7ecfced4f33b5676499e81acad66019cad98
NadetDev <nadetdev@gmail.com>
2023-11-12 20:23:04 +0100

    User model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'utilisateur avec email valide doit être valide' do
+    userEmailValide = User.new(email: 'user@usertest.com', password_digest: 'Pass123')
+    assert userEmailValide.valid?
+  end
+
+  test 'utilisateur avec email invalide doit être invalide' do
+    userEmailInvalide = User.new(email: 'user_usertest.com', password_digest: 'Pass123')
+    assert_not userEmailInvalide.valid?
+  end
+
+  test 'utilisateur avec email dupliqué est invalide' do
+    userEmailDuplique = User.new(email: 'admin@nadetdev.com', password_digest: 'Pass123')
+    assert_not userEmailDuplique.valid?
+  end
 end
