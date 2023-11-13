b3b4186783bf1780fb40526944d4cd8e2ff7aeeb
NadetDev <nadetdev@gmail.com>
2023-11-13 21:30:42 +0100

    Create new user method

--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -11,4 +11,21 @@ class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
   json_response = JSON.parse(self.response.body)
   assert_equal @user.email, json_response['email']
  end
+
+ test 'doit créer un utilisateur' do
+    assert_difference('User.count') do
+        post api_v1_users_url, params: { user: { email: 'test@test.com', password: 'Pass12345', 
+        password_confirmation: 'Pass12345' } }, as: :json
+    end
+    assert_response :created
 end
+
+test 'ne doit pas créer un nouvel utilisateur avec un email existant' do
+    assert_no_difference('User.count') do
+        post api_v1_users_url, params: { user: { email: @user.email, password: 'Pass123'}}, as: :json
+    end
+    assert_response :unprocessable_entity 
+end
+
+end
+
